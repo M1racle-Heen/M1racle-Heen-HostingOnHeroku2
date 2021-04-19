0407fb66aa5799599bf43cff0bece15a0975129c
M1racle-Heen <190109003@stu.sdu.edu.kz>
2021-04-19 13:24:18 +0600

    Added

--- routes/web.php
+++ routes/web.php
@@ -17,13 +17,16 @@ use App\Http\Controllers\MainController;
 */
 
 Route::get('/', function () {
-	App::setlocale('en');
-    return view('Master');
+    return redirect()->route('Index', ['param' => 'en']);
 });
 
-Route::get('locale/{locale}','App\Http\Controllers\MainController@changeLocale'
-)->name('locale');
+//Route::get('locale/{locale}','App\Http\Controllers\MainController@changeLocale'
+//)->name('locale');
 
+Route::get('locale/{locale?}', [
+   'uses' => 'App\Http\Controllers\MainController@changeLocale',
+   'as' => 'Index'
+]);
 Route::get('lang/{lang}', function($lang){
 	App::setlocale($lang);
 	return view('Master');
