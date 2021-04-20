7a753f143809bd6fb7f68c6ee5a6734dce84f20e
M1racle-Heen <190109003@stu.sdu.edu.kz>
2021-04-20 10:59:48 +0600

    Added

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -11,7 +11,7 @@ class MainController extends Controller
     	//\App::setlocale($locale);
     	//session()->put('locale',$locale);
     	//return redirect()->back();
-    	$currentLocale = App::getLocale();
+    	$currentLocale = \App::getLocale();
     	dd($currentLocale);
     }
 }
