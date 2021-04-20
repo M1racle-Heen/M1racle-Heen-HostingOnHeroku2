bea36c1233a296f6ba8882870fcf3cdec9eb4bcf
M1racle-Heen <190109003@stu.sdu.edu.kz>
2021-04-20 18:16:48 +0600

    Added

--- resources/views/Master.blade.php
+++ resources/views/Master.blade.php
@@ -76,7 +76,7 @@
                 <form action="upload" method="POST" enctype="multipart/form-data">
                    @csrf
                     <input type="file" name="file">
-                    <button type = "submit" onclick="window.location='{{ route('pSend',array("saidokma.228@gmail.com")) }}'">@lang('main.send_mail')</button>
+                    <button type = "submit" onclick="window.location='{{ route('pSend',array("190109003@stu.sdu.edu.kz")) }}'">@lang('main.send_mail')</button>
                 </form>
         </div>
         
