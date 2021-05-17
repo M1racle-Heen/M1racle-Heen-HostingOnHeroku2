596514b0d6544b2b8fbd39889a2ab0ffbc32c108
M1racle-Heen <190109003@stu.sdu.edu.kz>
2021-05-17 22:49:44 +0600

    Added

--- resources/views/Master.blade.php
+++ resources/views/Master.blade.php
@@ -30,13 +30,14 @@
   </div>
 	</div>
 	<header class="header">
-        <div class="hero">
-            <h1 class="title">@lang('main.food_for_health')</h1>
-             <button id = "btn_white1" type="button" onclick="popup1()" class="hero-button pulsate">@lang('main.book_a_table')</button>
-            <div class="user_profile">
-            	<p id ="user_name"></p>
-            </div>
-    </header>
+    <div class="hero">
+      <h1 class="title">@lang('main.food_for_health')</h1>
+      <button id = "btn_white1" type="button" onclick="popup1()" class="hero-button pulsate">@lang('main.book_a_table')</button>
+      <div class="user_profile">
+        <p id ="user_name"></p>
+      </div>
+    </div>
+  </header>
     <div class ="popup">  
     	<div class="popup-content">
     		<img class ="closed" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQREpNkaUqopqvFaEaPnEzWrqei9DMa5And2Q&usqp=CAU" width="7%" onclick="popout()">
@@ -438,6 +439,8 @@
      });
    </script>
 	-->
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/2.1.3/TimelineMax.min.js" integrity="sha512-0xrMWUXzEAc+VY7k48pWd5YT6ig03p4KARKxs4Bqxb9atrcn2fV41fWs+YXTKb8lD2sbPAmZMjKENiyzM/Gagw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/2.1.3/TweenMax.min.js" integrity="sha512-DkPsH9LzNzZaZjCszwKrooKwgjArJDiEjA5tTgr3YX4E6TYv93ICS8T41yFHJnnSmGpnf0Mvb5NhScYbwvhn2w==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
    <script src="{{ asset('js/onClickMenu.js') }}"></script>
 </body>
 </html> 
