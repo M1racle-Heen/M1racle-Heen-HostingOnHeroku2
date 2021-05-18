4e9ed74864d1618134a9fb6ef11087d957ad3e2e
M1racle-Heen <190109003@stu.sdu.edu.kz>
2021-05-18 16:23:50 +0600

    Added

--- public/js/onClickMenu.js
+++ public/js/onClickMenu.js
@@ -1,4 +1,23 @@
+var tl = new TimelineMax({onUpdate:updatePercentage});
+const controller = new ScrollMagic.Controller();
 
+tl.from(".menu-image",.5,{x:200,opacity: 0});
+tl.from(".menu-text-center",1,{width:0},"=-.7");
+
+
+const scene = new ScrollMagic.Scene({
+    triggerElement: ".menu",
+    triggerHook: "onLeave",
+    duration: "70%"
+})
+    .setPin(".menu")
+    .setTween(tl)
+    .addTo(controller);
+
+function updatePercentage(){
+    tl.progress();
+    console.log(tl.progress());
+}
 function onClickMenu() {
 	document.getElementById('menu-bar').classList.toggle('change');
 	document.getElementById('nav').classList.toggle('change-btn');
--- resources/views/Master.blade.php
+++ resources/views/Master.blade.php
@@ -140,7 +140,7 @@
            <div class="menu-text-center">
                <h1>@lang('main.our_menu')</h1>
                <p>@lang('main.our_menu2')</p>
-                <a onclick="clickMen()">@lang('main.explore')</a>
+               <a onclick="clickMen()">@lang('main.explore')</a>
            </div>
        </article>
    </section>
@@ -439,8 +439,11 @@
      });
    </script>
 	-->
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/2.1.3/TimelineMax.min.js" integrity="sha512-0xrMWUXzEAc+VY7k48pWd5YT6ig03p4KARKxs4Bqxb9atrcn2fV41fWs+YXTKb8lD2sbPAmZMjKENiyzM/Gagw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/2.1.3/TweenMax.min.js" integrity="sha512-DkPsH9LzNzZaZjCszwKrooKwgjArJDiEjA5tTgr3YX4E6TYv93ICS8T41yFHJnnSmGpnf0Mvb5NhScYbwvhn2w==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/2.0.2/TimelineMax.min.js" integrity="sha512-ExEP0u1SOp7gvzBoHWYmi6CEzdW2VHgfMeRZTetgX4HazJhC/UO1l83VGn4TLvgmIAxotN8uBcDE05a3sJglOA==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/2.0.2/TweenMax.min.js" integrity="sha512-O+atZ/gABlcFlpIoLQR04f72qHqZWKwTVsQZHqk/8VE2nrzDlnqhXbnXP7Gh/TyhpPT8tEUm5A580GG8AloHTA==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/ScrollMagic/2.0.7/ScrollMagic.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/ScrollMagic/2.0.7/plugins/animation.gsap.min.js"></script>
+    
   
    <script src="{{ asset('js/onClickMenu.js') }}"></script>
 </body>
